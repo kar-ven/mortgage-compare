78de3ac286ff6596b3c86a591ebc443f04441ec6
kar-ven <137957165+kar-ven@users.noreply.github.com>
2025-06-05 14:21:34 +0000

    add translator fix

--- translate.js
+++ translate.js
@@ -1,40 +1,31 @@
-function loadGoogleTranslate() {
-  var gt = document.createElement('script');
-  gt.type = 'text/javascript';
-  gt.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-  document.body.appendChild(gt);
+function loadGoogleTranslateScript(callback) {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = '//translate.google.com/translate_a/element.js?cb=' + callback;
+  document.head.appendChild(script);
 }
 
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({
     pageLanguage: 'en',
-    autoDisplay: false
+    includedLanguages: 'nl,de,fr',
+    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
   }, 'google_translate_element');
 }
 
+// Load the Google Translate script and init
+loadGoogleTranslateScript('googleTranslateElementInit');
+
 function translateTo(lang) {
-  if (!window.google || !window.google.translate) {
-    loadGoogleTranslate();
-    setTimeout(() => {
-      doTranslate(lang);
-    }, 1000); // wait for load
-  } else {
-    doTranslate(lang);
+  const langMap = {
+    dutch: 'nl',
+    german: 'de',
+    french: 'fr'
+  };
+  const select = document.querySelector('.goog-te-combo');
+  if (select) {
+    select.value = langMap[lang];
+    select.dispatchEvent(new Event('change'));
   }
 }
 
-function doTranslate(lang) {
-  const frame = document.querySelector('iframe.goog-te-menu-frame');
-  if (frame) {
-    const innerDoc = frame.contentDocument || frame.contentWindow.document;
-    const langLinks = innerDoc.querySelectorAll('.goog-te-menu2-item span.text');
-    for (let i = 0; i < langLinks.length; i++) {
-      if (langLinks[i].innerText.toLowerCase().includes(lang)) {
-        langLinks[i].click();
-        break;
-      }
-    }
-  } else {
-    setTimeout(() => doTranslate(lang), 500);
-  }
-}
